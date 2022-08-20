eb76fe18d83288a0577c05538a231f01ea9e6f92
Wojciech Lamperski <wolamp20@gmail.com>
2022-08-20 09:52:58 +0200

    added else if statements

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
-//the first alert
-alert("Hello, World");
+var youLikeMeat = true;
+var myNum = 10;
 
-/*
-the second alert
-*/
-alert("Hello, World");
+if (myNum > 10){
+    document.write("myNum is greater than 10. ");
+}else{
+    document.write("myNum is not greater than 10. ");
+}
+
+if (myNum == 10){
+    document.write("myNum is equal to 10. ");
+}else{
+    ddocument.write("myNum is not equal to 10. ");
+}
