51b48570a98f9b37e9d6b6177c5f5c96acf544dc
Wojciech Lamperski <wolamp20@gmail.com>
2022-08-21 18:06:08 +0200

    added coercion and explored math objects

--- test.js
+++ test.js
@@ -1,10 +1,19 @@
-function getAverage (a, b) {
+var a = 5;
+var b = 5;
 
-    var average = (a + b) / 2;
-    console.log(average);
-    return average;
+console.log(a + b);
+console.log(typeof(a + b));
 
-}
+b = "5";
 
-var myResult = getAverage(7,8);
-console.log("the average is " + myResult);
+console.log(a + b);
+console.log(typeof(a + b));
+
+//selcted functions and constans of the 'Math' object (there are many).
+
+console.log(Math.round(7.8));
+console.log(Math.flow(7.8));
+console.log(Math.ceil(7.2));
+console.log(Math.ceil(7.8));
+console.log(Math.max(7, 2, 4, 8));
+console.log(Math.PI);
