d3fb3d46640250694fc4271bfd3ff9725a6df188
Wojciech Lamperski <wolamp20@gmail.com>
2022-08-21 18:14:06 +0200

    implemented 'isNan()' function

--- test.js
+++ test.js
@@ -1,19 +1,9 @@
-var a = 5;
+var a = "apple";
 var b = 5;
 
-console.log(a + b);
-console.log(typeof(a + b));
+if(isNaN(a)){
+    console.log("that aint even a number, thickie")
+} else{
+    console.log("the meaning of life is " + (a * b));
+}
 
-b = "5";
-
-console.log(a + b);
-console.log(typeof(a + b));
-
-//selcted functions and constans of the 'Math' object (there are many).
-
-console.log(Math.round(7.8));
-console.log(Math.flow(7.8));
-console.log(Math.ceil(7.2));
-console.log(Math.ceil(7.8));
-console.log(Math.max(7, 2, 4, 8));
-console.log(Math.PI);
