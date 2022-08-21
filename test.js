08d0f26d23e1e42b3eca4aad4c827569828c3015
Wojciech Lamperski <wolamp20@gmail.com>
2022-08-21 16:22:24 +0200

    added a while loop

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
-var myAge =25;
-if(myAge < 18 || myAge > 30 || myAge === 25){
+var age = 5;
 
-    document.write("you aint coming!");
+while(age < 10){
 
-} else{
-
-    document.write("You can come, you cool dude");
-
+    console.log("Your age is less than 10");
+    age++;
 }
+
+document.write("you are now over 10");
