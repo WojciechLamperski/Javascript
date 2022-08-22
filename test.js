9d0fa63c686c5ce8a60b99ebeb3d6519aead0a4c
Wojciech Lamperski <wolamp20@gmail.com>
2022-08-22 22:21:01 +0200

    simplifying the code by adding constructor function

--- test.js
+++ test.js
@@ -1,18 +1,5 @@
-var myArray = new Array();
-myArray [0] = [8];
-myArray [1] = ["hello"];
 
-var myCar = new Object();
-myCar.maxSpeed = 120;
-myCar.driver = "Shaun";
-
-myCar.drive = function() { console.log("now driving");};
-
-myCar.drive();
-
-var myArray2 = [true, 15, "hi"];
-
-var myCar2 = {
+var myCar = {
     maxSpeed: 70, 
     driver: "Net Ninja", 
     distanceTraveled: function(speed, time) { 
@@ -23,6 +10,23 @@ var myCar2 = {
     } 
 };
 
-myCar2.logDriver();
-console.log(myCar2.maxSpeed);
-myCar2.distanceTraveled(50, 3);
+var Car = function(maxSpeed, driver){
+
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.distanceTraveled = function(speed, time){
+        console.log(speed * time);
+    };
+    this.longDriver = function(){
+        console.log("driver name is " + this.driver);
+    };
+
+};
+
+var myCar = new Car(50, "ninja");
+var myCar2 = new Car(50, "Humty Dumpty");
+var myCar3 = new Car(50, "Shaun");
+var myCar4 = new Car(50, "Wojtek");
+var myCar5 = new Car(50, "Mama");
+var myCar6 = new Car(50, "James Bond");
+var myCar7 = new Car(50, "Bruce Lee");
