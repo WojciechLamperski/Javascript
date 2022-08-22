24ededd5bdb355b11b4dacc673b52efb67c8f0ea
Wojciech Lamperski <wolamp20@gmail.com>
2022-08-22 22:59:37 +0200

    the date object

--- test.js
+++ test.js
@@ -1,32 +1,16 @@
 
-var myCar = {
-    maxSpeed: 70, 
-    driver: "Net Ninja", 
-    distanceTraveled: function(speed, time) { 
-        console.log(speed * time);
-    },
-    logDriver: function(){
-        console.log("driver name is " + this.driver);
-    } 
-};
+var birthday = new Date(1985, 0, 15, 11, 15, 25);
+var birthday2 = new Date(1985, 0, 15, 11, 15, 25);
 
-var Car = function(maxSpeed, driver){
+console.log(birthday.getMonth());
+console.log(birthday.getFullYear());
+console.log(birthday.getDate());
+console.log(birthday.getDay());
+console.log(birthday.getHours());
+console.log(birthday.getTime());
 
-    this.maxSpeed = maxSpeed;
-    this.driver = driver;
-    this.distanceTraveled = function(speed, time){
-        console.log(speed * time);
-    };
-    this.longDriver = function(){
-        console.log("driver name is " + this.driver);
-    };
-
-};
-
-var myCar = new Car(50, "ninja");
-var myCar2 = new Car(50, "Humty Dumpty");
-var myCar3 = new Car(50, "Shaun");
-var myCar4 = new Car(50, "Wojtek");
-var myCar5 = new Car(50, "Mama");
-var myCar6 = new Car(50, "James Bond");
-var myCar7 = new Car(50, "Bruce Lee");
+if(birthday.getTime() == birthday2.getTime()){
+    console.log("birtdays are equal");
+} else{
+    console.log("birtdays are not equal");
+}
