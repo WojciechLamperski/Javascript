b375066ff2ce80aa61e493444aacf558d5282d85
Wojciech Lamperski <wolamp20@gmail.com>
2022-08-26 21:12:19 +0200

    testing jquery

--- test.js
+++ test.js
@@ -1,23 +1,2 @@
-var colorChanger = document.querySelector("#color-changer");
-var colors = ["red", "blue", "green", "yellow"];
-var counter = 0;
-
-function changeColor(){
-
-    if(counter >= colors.length){
-        counter = 0;
-    }
-
-    colorChanger.style.background = colors[counter];
-    counter++;
-
-}
-
-var MyTimer = setInterval(changeColor, 3000);
-
-colorChanger.onclick = function(){
-
-    clearInterval(MyTimer);
-    colorChanger.innerHTML = "Timer stopped"
-
-}
+var myPara = document.querySelector("#content p:last-child");
+var myPara2 = $("#content p:last-child");
